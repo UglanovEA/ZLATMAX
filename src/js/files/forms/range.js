96bce832f40d4f9dbeb07c6def2745e5aca30600
UglanovEA <uglanovea@gmail.com>
2022-03-09 14:11:27 +0300

    ver 2.7

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -8,12 +8,14 @@ import * as noUiSlider from "nouislider";
 // import 'nouislider/dist/nouislider.css';
 
 export function rangeInit() {
-	const rangeItems = document.querySelectorAll("[data-range]");
-	if (rangeItems.length) {
-		rangeItems.forEach((rangeItem) => {
+	const rangePrices = document.querySelectorAll("[data-range-price]");
+	const rangeUnits = document.querySelectorAll("[data-range-units]");
+	if (rangePrices.length) {
+		rangePrices.forEach((rangeItem) => {
 			const fromValue = rangeItem.querySelector("[data-range-from]");
 			const toValue = rangeItem.querySelector("[data-range-to]");
 			const item = rangeItem.querySelector("[data-range-item]");
+			var inputs = [fromValue, toValue];
 			noUiSlider.create(item, {
 				start: [Number(fromValue.value), Number(toValue.value)], // [0,200000]
 				connect: true,
@@ -35,6 +37,48 @@ export function rangeInit() {
 			item.noUiSlider.on("update", function (values, handle) {
 				fromValue.value = parseInt(values[0]) + " руб.";
 				toValue.value = parseInt(values[1]) + " руб.";
+			});
+
+			inputs.forEach(function (input, handle) {
+				input.addEventListener("change", function () {
+					item.noUiSlider.setHandle(handle, this.value);
+				});
+			});
+		});
+	}
+	if (rangeUnits.length) {
+		rangeUnits.forEach((rangeItem) => {
+			const fromValue = rangeItem.querySelector("[data-range-from]");
+			const toValue = rangeItem.querySelector("[data-range-to]");
+			const item = rangeItem.querySelector("[data-range-item]");
+			var inputs = [fromValue, toValue];
+			noUiSlider.create(item, {
+				start: [Number(fromValue.value), Number(toValue.value)], // [0,200000]
+				connect: true,
+				step: 1,
+				tooltips: [true, true],
+				range: {
+					min: Number(fromValue.dataset.rangeFrom),
+					max: Number(toValue.dataset.rangeTo),
+				},
+				format: {
+					to: function (value) {
+						return parseInt(value);
+					},
+					from: function (value) {
+						return parseInt(value);
+					},
+				},
+			});
+			item.noUiSlider.on("update", function (values, handle) {
+				fromValue.value = parseInt(values[0]);
+				toValue.value = parseInt(values[1]);
+			});
+
+			inputs.forEach(function (input, handle) {
+				input.addEventListener("change", function () {
+					item.noUiSlider.setHandle(handle, this.value);
+				});
 			});
 		});
 	}
