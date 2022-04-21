3139399c213c12665e79c2f44a8e4f0e0182933b
UglanovEA <UglanovEA@gmail.com>
2022-04-21 16:46:16 +0300

    ver 3.9

--- src/js/app.js
+++ src/js/app.js
@@ -60,7 +60,7 @@ flsFunctions.tabs();
 Документация: https://template.fls.guru/template-docs/funkcional-popup.html
 Сниппет (HTML): pl
 */
-import "./libs/popup.js";
+// import "./libs/popup.js";
 
 /*
 Модуль параллакса мышью
